0bd9a89f9a13c4d92c6b149530de4558a3f57406
Thrasos <100732009+thrasosc@users.noreply.github.com>
2025-02-24 18:51:14 +0100

    implement animations in AbstractMythicalMob

--- common/src/main/java/net/pixeldreamstudios/mythos_bestiary/world/entity/mythical/AbstractMythicalMob.java
+++ common/src/main/java/net/pixeldreamstudios/mythos_bestiary/world/entity/mythical/AbstractMythicalMob.java
@@ -1,11 +1,39 @@
 package net.pixeldreamstudios.mythos_bestiary.world.entity.mythical;
 
+import mod.azure.azurelib.rewrite.util.MoveAnalysis;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.PathfinderMob;
 import net.minecraft.world.level.Level;
+import net.pixeldreamstudios.mythos_bestiary.world.entity.helper.AnimationDispatcher;
 
 public abstract class AbstractMythicalMob extends PathfinderMob {
+    public final AnimationDispatcher animationDispatcher;
+    public final MoveAnalysis moveAnalysis;
+
     protected AbstractMythicalMob(EntityType<? extends PathfinderMob> entityType, Level level) {
         super(entityType, level);
+        this.animationDispatcher = new AnimationDispatcher(this);
+        this.moveAnalysis = new MoveAnalysis(this);
+    }
+
+    @Override
+    public void tick() {
+        super.tick();
+        moveAnalysis.update();
+
+        if (this.level().isClientSide) {
+            var isMovingOnGround = moveAnalysis.isMovingHorizontally() && onGround();
+            Runnable animationRunner;
+            if (isMovingOnGround) {
+                if (this.isAggressive()) {
+                    animationRunner = animationDispatcher::run;
+                } else {
+                    animationRunner = animationDispatcher::walk;
+                }
+            } else {
+                animationRunner = animationDispatcher::idle;
+            }
+            animationRunner.run();
+        }
     }
 }
