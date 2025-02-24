54d9430f853dab2946abdf79f16ce382e2d921c9
Thrasos <100732009+thrasosc@users.noreply.github.com>
2025-02-24 18:47:07 +0100

    complete AnimationDispatcher

--- common/src/main/java/net/pixeldreamstudios/mythos_bestiary/world/entity/helper/AnimationDispatcher.java
+++ common/src/main/java/net/pixeldreamstudios/mythos_bestiary/world/entity/helper/AnimationDispatcher.java
@@ -2,6 +2,7 @@ package net.pixeldreamstudios.mythos_bestiary.world.entity.helper;
 
 import mod.azure.azurelib.rewrite.animation.dispatch.command.AzCommand;
 import mod.azure.azurelib.rewrite.animation.play_behavior.AzPlayBehaviors;
+import net.minecraft.world.entity.Entity;
 
 public class AnimationDispatcher {
     private static final AzCommand IDLE_COMMAND = AzCommand.create(
@@ -12,14 +13,31 @@ public class AnimationDispatcher {
 
     private static final AzCommand WALK_COMMAND = AzCommand.create(
             "base_controller",
-            "walking",
+            "walk",
             AzPlayBehaviors.LOOP
     );
 
     private static final AzCommand RUN_COMMAND = AzCommand.create(
             "base_controller",
-            "running",
+            "run",
             AzPlayBehaviors.LOOP
     );
 
+    private final Entity animatedEntity;
+
+    public AnimationDispatcher(Entity animatable) {
+        this.animatedEntity = animatable;
+    }
+
+    public void idle() {
+        IDLE_COMMAND.sendForEntity(animatedEntity);
+    }
+
+    public void walk() {
+        WALK_COMMAND.sendForEntity(animatedEntity);
+    }
+
+    public void run() {
+        RUN_COMMAND.sendForEntity(animatedEntity);
+    }
 }
