351d5ec4502800f0265c636009b2f09993db3ba5
Thrasos <100732009+thrasosc@users.noreply.github.com>
2025-02-24 21:57:24 +0100

    create AutoConfig screen on NeoForge

--- neoforge/src/main/java/net/pixeldreamstudios/mythos_bestiary/neoforge/MythosBestiaryNeoForgeClient.java
+++ neoforge/src/main/java/net/pixeldreamstudios/mythos_bestiary/neoforge/MythosBestiaryNeoForgeClient.java
@@ -1,12 +1,17 @@
 package net.pixeldreamstudios.mythos_bestiary.neoforge;
 
+import me.shedaniel.autoconfig.AutoConfig;
 import net.neoforged.api.distmarker.Dist;
+import net.neoforged.fml.ModLoadingContext;
 import net.neoforged.fml.common.Mod;
+import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 import net.pixeldreamstudios.mythos_bestiary.MythosBestiary;
+import net.pixeldreamstudios.mythos_bestiary.MythosBestiaryConfig;
 
 @Mod(value = MythosBestiary.MOD_ID, dist = Dist.CLIENT)
 public class MythosBestiaryNeoForgeClient {
         public MythosBestiaryNeoForgeClient() {
             MythosBestiary.initClient();
+            ModLoadingContext.get().registerExtensionPoint(IConfigScreenFactory.class, () -> (modContainer, screen) -> AutoConfig.getConfigScreen(MythosBestiaryConfig.class, screen).get());
         }
 }
