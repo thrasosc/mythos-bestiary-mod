bf9cdc7acc7e8d6d0df3389ab9f267c608d375f3
Thrasos <100732009+thrasosc@users.noreply.github.com>
2025-02-24 22:13:13 +0100

    improve config

--- common/src/main/java/net/pixeldreamstudios/mythos_bestiary/MythosBestiaryConfig.java
+++ common/src/main/java/net/pixeldreamstudios/mythos_bestiary/MythosBestiaryConfig.java
@@ -6,12 +6,15 @@ import me.shedaniel.autoconfig.annotation.ConfigEntry;
 
 @Config(name = MythosBestiary.MOD_ID)
 public class MythosBestiaryConfig implements ConfigData {
+    // Satyr
     @ConfigEntry.Category("satyr")
     @ConfigEntry.Gui.NoTooltip
-    public double satyrHealth = 10.0;
+    @ConfigEntry.BoundedDiscrete(min = 1, max = 100)
+    public int satyrHealth = 10;
     @ConfigEntry.Category("satyr")
     @ConfigEntry.Gui.NoTooltip
-    public double satyrAttackDamage = 1.5;
+    @ConfigEntry.BoundedDiscrete(min = 1, max = 20)
+    public int satyrAttackDamage = 2;
     @ConfigEntry.Category("satyr")
     @ConfigEntry.Gui.NoTooltip
     @ConfigEntry.BoundedDiscrete(max = 100)
