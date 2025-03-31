1dc296d0da7a3d51c3043e94d66d746bf3f997d9
thrasosc <100732009+thrasosc@users.noreply.github.com>
2025-04-01 01:27:22 +0200

    add basic goals

--- common/src/main/java/net/pixeldreamstudios/mythos_bestiary/entity/mythical/AbstractMythicalMob.java
+++ common/src/main/java/net/pixeldreamstudios/mythos_bestiary/entity/mythical/AbstractMythicalMob.java
@@ -3,6 +3,8 @@ package net.pixeldreamstudios.mythos_bestiary.entity.mythical;
 import mod.azure.azurelib.rewrite.util.MoveAnalysis;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.PathfinderMob;
+import net.minecraft.world.entity.ai.goal.*;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import net.pixeldreamstudios.mythos_bestiary.entity.helper.AnimationDispatcher;
 
@@ -14,6 +16,14 @@ public abstract class AbstractMythicalMob extends PathfinderMob {
         super(entityType, level);
         this.animationDispatcher = new AnimationDispatcher(this);
         this.moveAnalysis = new MoveAnalysis(this);
+    }
+
+    @Override
+    protected void registerGoals() {
+        this.goalSelector.addGoal(0, new FloatGoal(this));
+        this.goalSelector.addGoal(1, new WaterAvoidingRandomStrollGoal(this, 0.5));
+        this.goalSelector.addGoal(2, new LookAtPlayerGoal(this, Player.class, 6.0F));
+        this.goalSelector.addGoal(3, new RandomLookAroundGoal(this));
     }
 
     @Override
